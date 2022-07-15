256c5d8de3c2c893ee425d07495afbb71b2dcb7c
Randy Kim <rhkim1292@gmail.com>
2022-07-14 22:44:35 -0700

    Implement input validation for password confirm
    Javascript is used to check for equality between the values in the
    password field and the confirm password field.
    
    Styles were also added to valid inputs, invalid inputs, and inputs
    that are currently displaying the placeholder text to give the user
    feedback regarding the validity of their input.

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 const passwd = document.querySelector("#passwd");
+const confirmPasswd = document.querySelector("#confirmpasswd");
+
+let currPassword = "";
+let currConfirmPassword = "";
+let customValid = "";
+let customInvalid = "Password does not match!";
+
+function checkPasswordValidity() {
+	if (currPassword === currConfirmPassword) {
+		confirmPasswd.setCustomValidity(customValid);
+		return;
+	}
+
+	confirmPasswd.setCustomValidity(customInvalid);
+}
 passwd.addEventListener("input", (e) => {
-    console.log(e);
+	currPassword = e.target.value;
+	checkPasswordValidity();
+});
+
+confirmPasswd.addEventListener("input", (e) => {
+	currConfirmPassword = e.target.value;
+	checkPasswordValidity();
 });
